beed0cc5bb5f8a64c99561791102d3f46c06eff1
johnwquarles <john.w.quarles@gmail.com>
2015-06-02 22:07:04 -0500

    practice page more or less complete for now

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+var app = angular.module("angularPractice", []);
+
+app.controller('Main', function() {
+	var vm = this;
+
+	vm.things = [
+    {name: "A Folding Chair", twitter: "@haveaseat", photo: "http://i5.walmartimages.com/dfw/dce07b8c-ba1b/k2-_8a719131-8cf9-4423-b257-69c768e43202.v1.jpg"},
+    {name: "The Treble Cleff", twitter: "@highnotes", photo: "http://vignette1.wikia.nocookie.net/clubpenguin/images/6/66/Treble_Clef_Pin.PNG/revision/20150314211223"},
+    {name: "An Uninflated Beach Ball", twitter: "@novolleyball", photo: "http://2.bp.blogspot.com/-eqyKkKbPPU8/Ua6DIguQd1I/AAAAAAAAFyI/ioD_Ihm8Gjg/s1600/IMG_3291.JPG"}
+	];
+
+	vm.newThing = {};
+
+	vm.addNewThing = function(){
+		vm.things.push(vm.newThing);
+		vm.newThing = {};
+	};
+
+	vm.removeThing = function(thing) {
+    vm.things.splice(vm.things.indexOf(thing), 1);
+	};
+})
