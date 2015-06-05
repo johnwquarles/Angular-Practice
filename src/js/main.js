ef57424f268badae960483efb7a015dd7be26b5f
johnwquarles <john.w.quarles@gmail.com>
2015-06-05 02:31:57 -0500

    added firebase for data persistence; firebase turned data array into an object, which then needed to be filtered back into an array for use with filters over ng-repeat (wrote comments about this).

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,61 @@
 var app = angular.module("angularPractice", []);
 
-app.controller('Main', function() {
+app.filter('ransomcase', function() {
+  return function (string) {
+     return string.split('').map(function (char, i) {
+       return i % 2 ? char.toUpperCase(): char.toLowerCase();
+    }).join('')
+  }
+})
+
+// don't want to totally destroy the key; add it as an 'id' property of the objects contained in the resultant array.
+app.filter('objToArr', function() {
+  return function (obj) {
+    if (obj) {
+      return Object.keys(obj).map(function(key) {
+        // need quotes around _id?
+        obj[key]['_id'] = key;
+        return obj[key];
+      })
+    }
+  }
+})
+
+// pass in $http module as a dependency to the controller below.
+app.controller('Main', function($http) {
 	var vm = this;
 
-	vm.things = [
-    {name: "A Folding Chair", twitter: "@haveaseat", photo: "http://i5.walmartimages.com/dfw/dce07b8c-ba1b/k2-_8a719131-8cf9-4423-b257-69c768e43202.v1.jpg"},
-    {name: "The Treble Cleff", twitter: "@highnotes", photo: "http://vignette1.wikia.nocookie.net/clubpenguin/images/6/66/Treble_Clef_Pin.PNG/revision/20150314211223"},
-    {name: "An Uninflated Beach Ball", twitter: "@novolleyball", photo: "http://2.bp.blogspot.com/-eqyKkKbPPU8/Ua6DIguQd1I/AAAAAAAAFyI/ioD_Ihm8Gjg/s1600/IMG_3291.JPG"}
-	];
+  $http
+    .get('https://addressdiangular.firebaseio.com/things.json')
+    .success(function(data) {
+      vm.things = data;
+    });
+
+	// vm.things = [
+ //    {name: "A Folding Chair", twitter: "@haveaseat", photo: "http://i5.walmartimages.com/dfw/dce07b8c-ba1b/k2-_8a719131-8cf9-4423-b257-69c768e43202.v1.jpg"},
+ //    {name: "The Treble Cleff", twitter: "@highnotes", photo: "http://vignette1.wikia.nocookie.net/clubpenguin/images/6/66/Treble_Clef_Pin.PNG/revision/20150314211223"},
+ //    {name: "An Uninflated Beach Ball", twitter: "@novolleyball", photo: "http://2.bp.blogspot.com/-eqyKkKbPPU8/Ua6DIguQd1I/AAAAAAAAFyI/ioD_Ihm8Gjg/s1600/IMG_3291.JPG"}
+	// ];
 
 	vm.newThing = {};
 
 	vm.addNewThing = function(){
-		vm.things.push(vm.newThing);
-		vm.newThing = {};
-    $("#modal").modal('hide');
+    $http
+      .post('https://addressdiangular.firebaseio.com/things.json', vm.newThing)
+      .success(function(response){
+        vm.things[response.name] = vm.newThing;
+        vm.newThing = {};
+        vm.newThingForm.$setPristine();
+        $("#modal").modal('hide');
+      })
 	};
 
 	vm.removeThing = function(thing) {
-    vm.things.splice(vm.things.indexOf(thing), 1);
+    $http
+      .delete(`https://addressdiangular.firebaseio.com/things/${thing._id}.json`)
+      .success(function(){
+        delete vm.things[thing._id];
+      })
 	};
 
   vm.showLightbox = function(thing) {
